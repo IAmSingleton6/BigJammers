8fb3c1323ebe3a2456fa433a826d3ef931257c91
IAmSingleton6 <iamsingleton6@gmail.com>
2024-08-04 22:36:06 +0100

    Added static organisational scripts

---
+++ BigJammers/scripts/globals/collision_layers.gd
@@ -0,0 +1,8 @@
+class_name CollisionLayers
+
+enum CollisionLayer {
+	NONE = 0,
+	FIRE = 1,
+	WATER = 2,
+	ICE = 3,
+}
---
+++ BigJammers/scripts/globals/group_manager.gd
@@ -0,0 +1,23 @@
+class_name GroupManager
+
+# IMPORTANT
+# Add correct groups to correct area 2Ds/static bodies
+# Each block should have the block group
+# Each block should have its specific group 
+# Each level mechanic should have its specific group
+
+# Used to find the player in the scene or check for player in interaction
+static var PLAYERGROUP = "Player"
+
+# Used to check amount of blocks in the scene (apply to root of blocks)
+static var BLOCKGROUP = "Block"
+
+# Specific block types
+static var WATERGROUP = "Water"
+static var FIREGROUP = "Fire"
+static var TIMERGROUP = "Timer"
+static var ELECTRICGROUP = "Electric"
+
+# Level mechanics
+static var LASERGROUP = "Laser"
+static var WINDGROUP = "Wind"
