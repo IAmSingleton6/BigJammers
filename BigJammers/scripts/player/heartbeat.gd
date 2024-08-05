ca95b2c9585c409052873efb7a88460eb97c29fc
IAmSingleton6 <iamsingleton6@gmail.com>
2024-08-05 17:12:16 +0100

    Heartbeat sound plays and changes with health remaining

--- BigJammers/scripts/player/heartbeat.gd
+++ BigJammers/scripts/player/heartbeat.gd
@@ -3,11 +3,22 @@ class_name HeartBeat
 
 signal health_depleted
 signal damage_taken
-var health := 60
+
+@onready var audio_stream_player_heartbeat = $AudioStreamPlayer_heartbeat
+@onready var heartbeat_timer = $HeartbeatTimer
+
+@export var min_time_between_heartbeats := 0.5
+@export var max_time_between_heartbeats := 2.3
+@export var max_health := 60
+
+var health: float
 var reduce_health_with_time := false
 
 func _ready():
+	health = max_health
 	Events.level_start.connect(_on_level_start)
+	heartbeat_timer.timeout.connect(_on_heartbeat_timeout)
+	heartbeat_timer.start(_get_heartbeat_interval())
 
 
 func _on_level_start():
@@ -17,6 +28,7 @@ func _on_level_start():
 func _process(delta):
 	if reduce_health_with_time:
 		health -= delta
+		health = max(health, 0)
 	
 	if health <= 0:
 		health_depleted.emit()
@@ -26,3 +38,12 @@ func _process(delta):
 func take_damage(amount: float) -> void:
 	health -= amount
 	damage_taken.emit()
+
+
+func _get_heartbeat_interval() -> float:
+	return lerp(min_time_between_heartbeats, max_time_between_heartbeats, health / max_health)
+
+
+func _on_heartbeat_timeout() -> void:
+	audio_stream_player_heartbeat.play()
+	heartbeat_timer.start(_get_heartbeat_interval())
