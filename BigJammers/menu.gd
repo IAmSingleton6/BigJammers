85dad6f1f6b59c38d2f1419677225a2029d97c57
DerZwolfte <shabababusadat@gmail.com>
2024-08-09 21:05:51 +0100

    Menu in progress

--- BigJammers/menu.gd
+++ BigJammers/menu.gd
@@ -1,12 +1,38 @@
+class_name MainMenu
 extends Control
 
+@onready var play = $MarginContainer/HBoxContainer/VBoxContainer/Play as Button
+@onready var options = $MarginContainer/HBoxContainer/VBoxContainer/Options as Button
+@onready var exit = $MarginContainer/HBoxContainer/VBoxContainer/Exit as Button
+@onready var options_menu = $options_menu as OptionsMenu
+@onready var margin_container = $MarginContainer as MarginContainer
+
+@onready var first_level = preload("res://scenes/level_0.tscn")
+
+func _ready():
+	handle_connecting_signals()
 
 func _on_play_pressed():
 	print("Pressed Play")
-	SceneTransitions.change_scene_path("res://scenes/lvl_1.tscn")
+	SceneTransitions.change_scene_path(first_level)
 	
 func _on_options_pressed():
 	print("Pressed Options")
+	margin_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
 	
 func _on_exit_pressed():
 	get_tree().quit()
+	
+func on_exit_options_menu():
+	margin_container.visible = true
+	options_menu.visible = false
+	
+	 
+	
+func handle_connecting_signals():
+	play.pressed.connect(_on_play_pressed)
+	options.pressed.connect(_on_options_pressed)
+	exit.pressed.connect(_on_exit_pressed)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
