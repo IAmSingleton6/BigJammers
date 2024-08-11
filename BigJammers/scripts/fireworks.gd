6add81f4ba8dfcc8e6e6dc62391027a92913c066
IAmSingleton6 <iamsingleton6@gmail.com>
2024-08-11 13:49:30 +0100

    fireworks sounds added

--- BigJammers/scripts/fireworks.gd
+++ BigJammers/scripts/fireworks.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var _1 = $"1"
 @onready var _2 = $"2"
 @onready var _3 = $"3"
-@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _ready():
@@ -17,10 +17,16 @@ func _ready():
 	global_position = pos
 	
 	_1.emitting = true
+	audio_stream_player_2d.global_position = _1.global_position
+	audio_stream_player_2d.pitch_scale = randf_range(0.8, 1.2)
 	audio_stream_player_2d.play()
 	await get_tree().create_timer(0.7).timeout
 	_2.emitting = true
+	audio_stream_player_2d.global_position = _2.global_position
+	audio_stream_player_2d.pitch_scale = randf_range(0.8, 1.2)
 	audio_stream_player_2d.play()
 	await get_tree().create_timer(0.9).timeout
 	_3.emitting = true
+	audio_stream_player_2d.global_position = _3.global_position
+	audio_stream_player_2d.pitch_scale = randf_range(0.8, 1.2)
 	audio_stream_player_2d.play()
