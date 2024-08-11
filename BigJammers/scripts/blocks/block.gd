1ab055bb21d0e289b8b5f4978611aa08100067c8
IAmSingleton6 <iamsingleton6@gmail.com>
2024-08-11 17:27:41 +0100

    added laser sound pew pew

--- BigJammers/scripts/blocks/block.gd
+++ BigJammers/scripts/blocks/block.gd
@@ -8,12 +8,15 @@ extends CollisionShape2D
 func destroy_block(group_name: String) -> void:
 	if group_name == GroupManager.LASERGROUP:
 		add_child(laser_audio_player)
+		laser_audio_player.reparent(get_parent().get_parent())
 		laser_audio_player.stream = load("res://assets/audio/sfx/laser.wav")
+		laser_audio_player.volume_db = -7
 		laser_audio_player.play()
 		
 		var particles = particles_on_laser_destroy.instantiate()
 		get_parent().get_parent().add_child(particles)
 		await get_tree().process_frame
+		laser_audio_player.global_position = global_position
 		particles.global_position = global_position
 		
 		print("block distinguishes by laser")
