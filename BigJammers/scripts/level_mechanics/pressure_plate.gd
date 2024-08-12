bcbe3ed6ec1459cebc676c9a1acf483eeed0a899
IAmSingleton6 <iamsingleton6@gmail.com>
2024-08-12 01:30:43 +0100

    pressure plate now can have exact amount or any over to activate

--- BigJammers/scripts/level_mechanics/pressure_plate.gd
+++ BigJammers/scripts/level_mechanics/pressure_plate.gd
@@ -6,6 +6,8 @@ signal pressure_plate_activated
 signal pressure_plate_deactivated
 
 @onready var label: Label = $WeightText/Label
+
+@export var EXACT_AMOUNT_NEEDED := true
 @export var BLOCKCOUNT := 2
 @export var ONETIMEINTERACTION := false
 
@@ -37,7 +39,7 @@ func _on_body_entered(other):
 	stepped_on = true
 	var block_count := _get_block_count()
 	_update_label(BLOCKCOUNT - block_count)
-	if not block_count == BLOCKCOUNT:
+	if (EXACT_AMOUNT_NEEDED and not block_count == BLOCKCOUNT) or (not EXACT_AMOUNT_NEEDED and not block_count >= BLOCKCOUNT):
 		return
 	
 	pressure_plate_activated.emit()
@@ -62,11 +64,14 @@ func _get_block_count() -> int:
 
 
 func _update_label(count: int) -> void:
-	var text = str(count) if count >= 0 else "ERROR"
+	var text = str(count) if count > 0 else "ERROR"
+	text = "Activated" if count == 0 or count < 0 and not EXACT_AMOUNT_NEEDED else text
+	text = "Error" if count < 0 and EXACT_AMOUNT_NEEDED else text
 	label.text = text
-	if count == 0:
+	
+	if count == 0 or count < 0 and not EXACT_AMOUNT_NEEDED:
 		label.self_modulate = Color.GREEN
-	elif count < 0:
+	elif count < 0 and EXACT_AMOUNT_NEEDED:
 		label.self_modulate = Color.RED
 	else:
 		label.self_modulate= Color.WHITE
